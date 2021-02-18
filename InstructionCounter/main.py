ee9ac376c879fd9aa9ab01e8d112c4fc51e2ec1f
Jacob Nørhave <jnarha16@student.aau.dk>
2021-02-18 13:56:06 +0100

    Added -l, --list to list available methods

--- InstructionCounter/main.py
+++ InstructionCounter/main.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', type=argparse.FileType('r'), help='Counts all of the instructions used', required=True)
     parser.add_argument('-m', '--method', type=str, help='Countes the instructions for the specific method')
+    parser.add_argument('-l', '--list', action='store_true', help='Will print a list of available methods')
     parser.add_argument('-o', '--output', type=str, default='results.csv', help='The name of the output file (Default = "results.csv")')
     args = parser.parse_args()
 
@@ -24,18 +25,21 @@ if __name__ == '__main__':
     ## Split all code into methods
     instructions = yamlclass.load('instructions.yaml')
     methods = utilities.get_by_method(text)
-    if args.method:
-        found = False
-        for index, m in enumerate(methods):
-            if args.method == m.name:
-                found = True
-                if m.arguments:
-                    raise Exception(f"The chosen method requires arguments, and can therefore not be counted by itself")
-                res = methods[index].get_instructions(instructions)
-                write_to_file(m.name, res, args.output)
-        if not found:
-            raise Exception(f"The specified method '{args.method}' was not found. Please look at the available options: {[x.name for x in methods]}")
+    if args.list:
+        print(f'The available methods are as follows: {[x.name for x in methods]}')
     else:
-        for m in methods:
-            res = m.get_instructions(instructions)
-            write_to_file(m.name, res, args.output)
+        if args.method:
+            found = False
+            for index, m in enumerate(methods):
+                if args.method == m.name:
+                    found = True
+                    if m.arguments:
+                        raise Exception(f"The chosen method requires arguments, and can therefore not be counted by itself")
+                    res = methods[index].get_instructions(instructions)
+                    write_to_file(m.name, res, args.output)
+            if not found:
+                raise Exception(f"The specified method '{args.method}' was not found. Please look at the available options: {[x.name for x in methods]}")
+        else:
+            for m in methods:
+                res = m.get_instructions(instructions)
+                write_to_file(m.name, res, args.output)
