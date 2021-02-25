584c9b176bc3a921b05c21a31160f9b0ea3a2a37
Jacob Nørhave <jnarha16@student.aau.dk>
2021-02-25 13:32:33 +0100

    Add repr for the variable class

--- InstructionCounter/variable.py
+++ InstructionCounter/variable.py
@@ -5,8 +5,14 @@ class variable():
         self.type = type
         self.value = None
 
+
     def set_value(self, value):
         self.value = value
 
+
     def get_value(self):
         return self.value
+
+
+    def __repr__(self) -> str:
+        return f"('{self.value}' - {self.type})"
