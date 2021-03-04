4ec8875393997c5e1df61b6527f4d6a9c5cdc143
Anne Ejsing <anneejsing77@gmail.com>
2021-03-04 20:01:10 +0100

    Cleanup train.py

--- MLApproach/train.py
+++ MLApproach/train.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy import stats
 from sklearn import preprocessing
 from sklearn.model_selection import KFold, RepeatedKFold
-from sklearn.linear_model import LinearRegression, Lasso, Ridge, RidgeCV
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, RidgeCV, LassoCV
 import os
 import pickle
 
@@ -22,19 +22,16 @@ X = pd.DataFrame(
     df.drop(['name', 'pkg(µj)', 'duration(ms)', 'dram(µj)', 'temp(C)'], axis=1))
 y = pd.DataFrame(df['pkg(µj)'])
 
-# LinearRegression() creates a linear regression model and the for loop divides
-# the dataset into three folds (by shuffling its indices). Inside the loop,
-# we fit the data and then assess its performance by appending its score to a
-# list (scikit-learn returns the R² score which is the coefficient of determination).
+print(y.max() / 1000000)  # pkg(µj)    21834145581 µj  = 21834.145581 j
+print(y.min() / 1000000)  # pkg(µj)    30335 µj        = 0.030335 j
+print(y.mean() / 1000000) # pkg(µj)    2.817638e+08 µj = 281.763764 j
+
+
 #model = LinearRegression()
-model = Lasso(alpha=1.0)
-
 cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
-# RidgeCV: version of the algorithm that automatically finds good hyperparameters
-#model = RidgeCV(alphas=np.arange(0, 1, 0.01), cv=cv, scoring='neg_mean_absolute_error')
-model.fit(X, y)
-f = open('model.obj','wb')
-pickle.dump(model, f)
+#model = LassoCV(alphas=np.arange(0, 1, 0.01), cv=cv, n_jobs=-1)
+model = RidgeCV(alphas=np.arange(0, 1, 0.01), cv=cv, scoring='neg_mean_absolute_error')
 
-# Score is R^2 value
-score = model.score(X, y)
+
+#model.fit(X, y)
+#pickle.dump(model, open('model.obj','wb'))
