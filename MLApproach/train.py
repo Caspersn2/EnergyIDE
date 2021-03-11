5bfc57f08e886dea330c0638762f3bd884324d9b
Anne Ejsing <anneejsing77@gmail.com>
2021-03-11 08:37:29 +0100

    Add more regression methods to training. Also adds cross validation

--- MLApproach/train.py
+++ MLApproach/train.py
@@ -1,40 +1,58 @@
 import pandas as pd
 import numpy as np
-from scipy import stats
-from sklearn import preprocessing
-from sklearn.model_selection import KFold, RepeatedKFold
-from sklearn.linear_model import LinearRegression, Lasso, Ridge, RidgeCV
+from sklearn.model_selection import RepeatedKFold
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from sklearn.model_selection import cross_val_score
 import os
-
-# Merge tempResults.csv and training.csv
-if not os.path.exists('output.csv'):
-    a = pd.read_csv("tempResults.csv", sep=';')
-    b = pd.read_csv("training.csv")
-    b = b.dropna(axis=1)
-    df = a.merge(b, on='name')
-    df.to_csv("output.csv", index=False)
-else:
-    df = pd.read_csv('output.csv')
+import argparse
+import pickle
 
 
-X = pd.DataFrame(
-    df.drop(['name', 'pkg(µj)', 'duration(ms)', 'dram(µj)', 'temp(C)'], axis=1))
-y = pd.DataFrame(df['pkg(µj)'])
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--regression-method', choices=['Linear', 'Ridge', 'Lasso', 'RandomForest', 'SVR'], default='Linear', help='Determine the regression algorithm used')
+    parser.add_argument('-s', '--save-model', action='store_true',help='Will save the model using pickle')
+    parser.add_argument('-c', '--cross-validate', action='store_true',help='Will perform cross validation of the chosen regression model')
+    args = parser.parse_args()
 
-# LinearRegression() creates a linear regression model and the for loop divides
-# the dataset into three folds (by shuffling its indices). Inside the loop,
-# we fit the data and then assess its performance by appending its score to a
-# list (scikit-learn returns the R² score which is the coefficient of determination).
-#model = LinearRegression()
-model = Lasso(alpha=1.0)
+    # Merge tempResults.csv and training.csv
+    if not os.path.exists('output.csv'):
+        a = pd.read_csv("tempResults.csv", sep=';')
+        b = pd.read_csv("training.csv")
+        b = b.dropna(axis=1)
+        df = a.merge(b, on='name')
+        df.to_csv("output.csv", index=False)
+    else:
+        df = pd.read_csv('output.csv')
 
-cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
-# RidgeCV: version of the algorithm that automatically finds good hyperparameters
-#model = RidgeCV(alphas=np.arange(0, 1, 0.01), cv=cv, scoring='neg_mean_absolute_error')
-model.fit(X, y)
 
-# Score is R^2 value
-score = model.score(X, y)
-print(score)
-print(model.coef_)
-print(model.intercept_)
+    X = pd.DataFrame(
+        df.drop(['name', 'pkg(µj)', 'duration(ms)', 'dram(µj)', 'temp(C)'], axis=1))
+    y = pd.DataFrame(df['pkg(µj)'])
+
+    model = None
+
+    if args.regression_method == 'Lasso':
+        model = Lasso()
+    elif args.regression_method == 'Ridge':
+        model = Ridge()
+    elif args.regression_method == 'RandomForest':
+        model = RandomForestRegressor(max_depth=10, random_state=0)
+        y = np.ravel(y)
+    elif args.regression_method == 'SVR':
+        model = SVR(kernel='rbf', C=1e3, gamma=0.1)
+        y = np.ravel(y)
+    else:
+        model = LinearRegression()
+
+    
+    model.fit(X, y)
+    print(model.score(X, y))
+    if args.cross_validate:
+        splits = int(len(y) /10)
+        print(np.mean(cross_val_score(model, X, y, cv=splits,scoring='neg_root_mean_squared_error')))
+
+    if args.save_model:
+        pickle.dump(model, open('model.obj','wb'))
