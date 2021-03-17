55ca127c2e5750a3ca8064e2176924c0f506036d
Jacob Nørhave <jnarha16@student.aau.dk>
2021-03-17 14:43:03 +0100

    Changed simulation entry and minor cleanup
    * Main has been cleaned to make it much more readable
    * Method is no longer the entry when simulating
    * State Machine is now to class which starts a simulation
    * Created custom exception to please SonarLint
    MORE CLEANUP IS EXCEPTED TO COME

--- MLApproach/InstructionCounter/method.py
+++ MLApproach/InstructionCounter/method.py
@@ -16,39 +16,10 @@ class method():
         self.locals = utilities.get_local_stack(text)
         self.data = instruction.get_all_instructions(self.text)
 
+    
+    def get_instructions(self):
+        return self.data
 
-    def count_instructions(self, instructions):
-        counter = {}
-        for inst in instructions:
-            if inst.name not in counter:
-                counter[inst.name] = 0
-            counter[inst.name] += 1
-        return counter
-
-
-    def get_instructions(self, available_methods, active_class):
-        machine = state_machine(available_methods)
-        machine.load_arguments(self.arguments)
-        machine.load_locals(self.locals)
-        machine.is_instance(self.is_instance)
-
-        if active_class:
-            machine.load_active_class(active_class)
-        else:
-            machine.load_active_class(copy.deepcopy(self.cls))
-
-        instructions = list(self.data.keys())
-        index = 0
-        return_val = None
-        while index != len(instructions):
-            current = instructions[index]
-            action, value = machine.simulate(self.data[current])
-            if action == Actions.JUMP:
-                index = instructions.index(value)
-            elif action == Actions.NOP:
-                index += 1
-            elif action == Actions.RETURN:
-                return_val = value
-                break
-
-        return self.count_instructions(machine.executed), return_val
+    
+    def get_class(self):
+        return copy.deepcopy(self.cls)
