dc15ebaf8dab083eebd01122813307b62cb07fc2
Anne Ejsing <anneejsing77@gmail.com>
2021-03-25 09:14:33 +0100

    Make extension code more generic

--- EnergyExtension/src/messageParsers/MeasurePasers.ts
+++ EnergyExtension/src/messageParsers/MeasurePasers.ts
@@ -2,11 +2,10 @@ import * as vscode from 'vscode';
 import { WebviewView } from "vscode";
 import { MeasureTestingService } from "../service/measure-testing.service";
 
-export class Measure 
-{
-    static getMethods(webviewView: WebviewView) {
+export class Measure {
+    static getMethods(webviewView: WebviewView, type: string) {
         vscode.workspace.findFiles('**/*.dll').then(files => {
-            MeasureTestingService.getMethods(files.map(f => f.fsPath)).then(methods => {
+            MeasureTestingService.getMethods(files.map(f => f.fsPath), type).then(methods => {
                 webviewView.webview.postMessage({ command: 'methods', value: methods });
             });
         });
@@ -21,19 +20,19 @@ export class Measure
     }
 
     static delay(ms: number) {
-        return new Promise( resolve => setTimeout(resolve, ms) );
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 
     static stopProgress: boolean = false;
     static startProgressListen(webviewview: vscode.WebviewView) {
-        MeasureTestingService.checkStatus().then(status => {
-            if (typeof(status) !== typeof(String)){
+        MeasureTestingService.checkRAPLStatus().then(status => {
+            if (typeof (status) !== typeof (String)) {
                 status = <MeasureProgess>status;
                 // If the measurements are done, stop listen and call the webview with done
                 if (status.Done) {
                     this.stopProgress = status.Done;
                     webviewview.webview.postMessage({ command: 'done', value: status });
-                    this.openOutput( status.Output == undefined ? "Could not read the output." : status.Output );
+                    this.openOutput(status.Output == undefined ? "Could not read the output." : status.Output);
                 }
                 else {
                     webviewview.webview.postMessage({ command: 'progress', value: status });
@@ -47,16 +46,14 @@ export class Measure
         });
     }
 
-    static activate(message: ActivateClass, webviewView: vscode.WebviewView) {
-        var ids: number[] = [];
-        message.methods?.forEach(m => {
-            if (m.id){
-                ids.push(m.id);
-            }
-        });
-        
-        if (message.methods != null) {
-            MeasureTestingService.startRunning(ids).then(response => {
+    static activate(activeClasses: ActivateClass[], type: string, webviewView: vscode.WebviewView) {
+        if (type === "rapl") {
+            var ids: number[] = [];
+            activeClasses.forEach(c => {
+                c.Methods?.forEach(m => ids.push(m.Id as number));
+            });
+
+            MeasureTestingService.startRAPL(ids).then(response => {
                 if (response) {
                     //Starting to listen to progress
                     this.stopProgress = false;
@@ -64,35 +61,41 @@ export class Measure
                 }
             });
         }
+        else if (type === "ml") {
+            MeasureTestingService.startML(activeClasses);
+        }
+        //else if (type === "energy_model")
+
     }
 
     static stop() {
-        MeasureTestingService.stopRunning().then(response => {
+        MeasureTestingService.stopRAPL().then(response => {
             console.log("Stopped: " + response);
         });
     }
 }
 
-export class ActivateClass {
-    type: string | undefined;
-    methods: Method[] | undefined;
+export interface ActivateClass {
+    ClassName: string | undefined;
+    AssemblyPath: string | undefined;
+    Methods: Method[] | undefined;
 }
 
-export class Method {
-    id: number | undefined;
-    name: string | undefined;
+export interface Method {
+    Id: number | undefined;
+    Name: string | undefined;
 }
 
-export class MeasureProgess {
+export interface MeasureProgess {
     PlannedMethods: Array<MethodProgress> | undefined;
     ClassesPlanned: Array<string> | undefined;
     ExceptionString: string | undefined;
-    ExceptionThrown: boolean| undefined;
+    ExceptionThrown: boolean | undefined;
     Done: boolean | undefined;
     Output: string | undefined;
 }
 
-export class MethodProgress {
+export interface MethodProgress {
     id: Number | undefined;
     runsDone: Number | undefined;
     plannedRuns: Number | undefined;
