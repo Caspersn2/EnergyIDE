5e7521f6e7d141d8eaa1b8f514a77db305339051
Jacob Nørhave <jnarha16@student.aau.dk>
2021-03-30 20:33:50 +0200

    Update result to contain and print return values for each method

--- MLApproach/InstructionCounter/result.py
+++ MLApproach/InstructionCounter/result.py
@@ -1,7 +1,7 @@
 result_store = []
 
 
-def add_results(results, method, counting_type='Simple'):
+def add_results(results, method, counting_type='Simple', return_value = None):
     name = method.name
     args = None
     if counting_type == 'Simulation':
@@ -11,7 +11,8 @@ def add_results(results, method, counting_type='Simple'):
     single_result = {
         'name': name,
         'args': args,
-        'res': results
+        'res': results,
+        'return': return_value
     }
 
     result_store.append(single_result)
@@ -28,9 +29,13 @@ def output(output):
                 data_file.write(result['name'])
 
                 if result['args']:
-                    data_file.write(f' --> {result["args"]}')
+                    data_file.write(f'\nARGUMENTS: {result["args"]}')
+
+                if 'return' in result and result['return'] == 0 or result['return']:
+                    data_file.write(f'\nRETURNS: {result["return"]}')
 
                 data_file.write('\n')
+                data_file.write('=' * 30 + '\n')
                 for (k, v) in result['res'].items():
                     data_file.write(f'{k},{v}\n')
                 data_file.write('\n')
